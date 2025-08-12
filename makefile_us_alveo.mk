f1d6a10403082c8bc06664f526de0640cc6bded5
chiboubnour <chiboubnour32@gmail.com>
2025-08-12 15:01:35 +0200

    Update makefile_us_alveo.mk

--- makefile_us_alveo.mk
+++ makefile_us_alveo.mk
@@ -1,62 +1,146 @@
-# Platform target (U280)
-PLATFORM ?= xilinx_u280_gen3x16_xdma_1_202211_1
+#
+# Copyright 2019-2021 Xilinx, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# makefile-generator v1.0.3
+#
 
-# Kernel name
-KERNEL := krnl_hash_dna
+############################## Help Section ##############################
+ifneq ($(findstring Makefile, $(MAKEFILE_LIST)), Makefile)
+help:
+	$(ECHO) "Makefile Usage:"
+	$(ECHO) "  make all TARGET=<hw_emu/hw> PLATFORM=<FPGA platform>"
+	$(ECHO) "      Command to generate the design for specified Target and Shell."
+	$(ECHO) ""
+	$(ECHO) "  make run TARGET=<hw_emu/hw> PLATFORM=<FPGA platform>"
+	$(ECHO) "      Command to run application in emulation."
+	$(ECHO) ""
+	$(ECHO) "  make build TARGET=<hw_emu/hw> PLATFORM=<FPGA platform>"
+	$(ECHO) "      Command to build xclbin application."
+	$(ECHO) ""
+	$(ECHO) "  make host"
+	$(ECHO) "      Command to build host application."
+	$(ECHO) ""
+	$(ECHO) "  make clean "
+	$(ECHO) "      Command to remove the generated non-hardware files."
+	$(ECHO) ""
+	$(ECHO) "  make cleanall"
+	$(ECHO) "      Command to remove all the generated files."
+	$(ECHO) ""
 
-# Source files
-HOST_SRC := src/host.cpp
-KERNEL_SRC := src/krnl_hash.cpp
-CONFIG_FILE := krnl_hash.cfg
+endif
 
-# Build targets
-BUILD_DIR := build_dir.$(TARGET).$(PLATFORM)
-XO_FILE := $(BUILD_DIR)/$(KERNEL).xo
-XCLBIN_FILE := $(BUILD_DIR)/$(KERNEL).xclbin
+############################## Setting up Project Variables ##############################
+TARGET := hw
+VPP_LDFLAGS :=
+include ./utils.mk
 
-# Compiler options
-CXXFLAGS := -Wall -O2 -g -std=c++17
-LDFLAGS := -lxrt_coreutil -luuid -lpthread
+TEMP_DIR := ./_x.$(TARGET).$(XSA)
+BUILD_DIR := ./build_dir.$(TARGET).$(XSA)
 
-# Xilinx tools
-VPP := v++
-CXX := g++
+LINK_OUTPUT := $(BUILD_DIR)/krnl_hach.link.xclbin
+PACKAGE_OUT = ./package.$(TARGET)
 
-# Default target
-all: build
+VPP_PFLAGS := 
+CMD_ARGS = -x $(BUILD_DIR)/krnl_hach.xclbin
+CXXFLAGS += -I$(XILINX_XRT)/include -I$(XILINX_VIVADO)/include -Wall -O0 -g -std=c++17
+LDFLAGS += -L$(XILINX_XRT)/lib -pthread -lOpenCL
 
-# Step 1: Compile kernel to XO
-$(XO_FILE): $(KERNEL_SRC)
-	$(VPP) -c -t $(TARGET) --platform $(PLATFORM) \
-		--save-temps -g \
-		--config $(CONFIG_FILE) \
-		-k $(KERNEL) \
-		--temp_dir $(BUILD_DIR) \
-		--output $(XO_FILE) $<
+########################## Checking if PLATFORM in allowlist #######################
+PLATFORM_BLOCKLIST += u25_ u30 u200 zc vck u250 aws-vu9p-f1 samsung u2_ x3522pv nodma v70 
+############################## Setting up Host Variables ##############################
+#Include Required Host Source Files
+CXXFLAGS += -I$(XF_PROJ_ROOT)/common/includes/cmdparser
+CXXFLAGS += -I$(XF_PROJ_ROOT)/common/includes/logger
+HOST_SRCS += $(XF_PROJ_ROOT)/common/includes/cmdparser/cmdlineparser.cpp $(XF_PROJ_ROOT)/common/includes/logger/logger.cpp ./src/host.cpp 
+# Host compiler global settings
+CXXFLAGS += -fmessage-length=0
+LDFLAGS += -lrt -lstdc++ 
+LDFLAGS += -luuid -lxrt_coreutil
 
-# Step 2: Link XO to XCLBIN
-$(XCLBIN_FILE): $(XO_FILE)
-	$(VPP) -l -t $(TARGET) --platform $(PLATFORM) \
-		--save-temps -g \
-		--config $(CONFIG_FILE) \
-		--temp_dir $(BUILD_DIR) \
-		--output $(XCLBIN_FILE) $(XO_FILE)
+############################## Setting up Kernel Variables ##############################
+# Kernel compiler global settings
+VPP_FLAGS += --save-temps 
 
-# Step 3: Compile host
-host: $(HOST_SRC)
-	$(CXX) $(CXXFLAGS) -o host $(HOST_SRC) $(LDFLAGS)
 
-# Build everything
-build: $(XCLBIN_FILE) host
+# Kernel linker flags
+VPP_LDFLAGS_krnl_hach += --config ./krnl_hach.cfg
+EXECUTABLE = ./hach_example
+EMCONFIG_DIR = $(TEMP_DIR)
 
-# Run on board
-run: build
-	XCL_EMULATION_MODE=$(TARGET) ./host $(XCLBIN_FILE)
+############################## Setting Targets ##############################
+.PHONY: all clean cleanall docs emconfig
+all: check-platform check-device check-vitis $(EXECUTABLE) $(BUILD_DIR)/krnl_hach.xclbin emconfig
 
-# Clean generated files
+.PHONY: host
+host: $(EXECUTABLE)
+
+.PHONY: build
+build: check-vitis check-device $(BUILD_DIR)/krnl_hach.xclbin
+
+.PHONY: xclbin
+xclbin: build
+
+############################## Setting Rules for Binary Containers (Building Kernels) ##############################
+$(TEMP_DIR)/krnl_hach.xo: src/krnl_hach.cpp
+	mkdir -p $(TEMP_DIR)
+	v++ -c $(VPP_FLAGS) -t $(TARGET) --platform $(PLATFORM) -k krnl_hach --temp_dir $(TEMP_DIR)  -I'$(<D)' -o'$@' '$<'
+
+$(BUILD_DIR)/krnl_hach.xclbin: $(TEMP_DIR)/krnl_hach.xo
+	mkdir -p $(BUILD_DIR)
+	v++ -l $(VPP_FLAGS) $(VPP_LDFLAGS) -t $(TARGET) --platform $(PLATFORM) --temp_dir $(TEMP_DIR) $(VPP_LDFLAGS_krnl_hach) -o'$(LINK_OUTPUT)' $(+)
+	v++ -p $(LINK_OUTPUT) $(VPP_FLAGS) -t $(TARGET) --platform $(PLATFORM) --package.out_dir $(PACKAGE_OUT) -o $(BUILD_DIR)/krnl_hach.xclbin
+
+############################## Setting Rules for Host (Building Host Executable) ##############################
+$(EXECUTABLE): $(HOST_SRCS) | check-xrt
+		g++ -o $@ $^ $(CXXFLAGS) $(LDFLAGS)
+
+emconfig:$(EMCONFIG_DIR)/emconfig.json
+$(EMCONFIG_DIR)/emconfig.json:
+	emconfigutil --platform $(PLATFORM) --od $(EMCONFIG_DIR)
+
+############################## Setting Essential Checks and Running Rules ##############################
+run: all
+ifeq ($(TARGET),$(filter $(TARGET),hw_emu))
+	cp -rf $(EMCONFIG_DIR)/emconfig.json .
+	XCL_EMULATION_MODE=$(TARGET) $(EXECUTABLE) $(CMD_ARGS)
+else
+	$(EXECUTABLE) $(CMD_ARGS)
+endif
+ifneq ($(TARGET),$(findstring $(TARGET), hw hw_emu))
+$(error Software Emulation is not supported for the examples and will be deprecated in the tool in 2024.2. Please run hw_emu or hw target.)
+endif
+
+.PHONY: test
+test: $(EXECUTABLE)
+ifeq ($(TARGET),$(filter $(TARGET),hw_emu))
+	XCL_EMULATION_MODE=$(TARGET) $(EXECUTABLE) $(CMD_ARGS)
+else
+	$(EXECUTABLE) $(CMD_ARGS)
+endif
+ifneq ($(TARGET),$(findstring $(TARGET), hw hw_emu))
+$(warning WARNING:Software Emulation is not supported for the examples and will be deprecated in the tool in 2024.2. Please run hw_emu or hw target.)
+endif
+
+
+############################## Cleaning Rules ##############################
+# Cleaning stuff
 clean:
-	rm -rf build_dir.* *.xclbin *.xo *.log *.info *.wdb host
+	-$(RMDIR) $(EXECUTABLE) $(XCLBIN)/{*hw_emu*} 
+	-$(RMDIR) profile_* TempConfig system_estimate.xtxt *.rpt *.csv 
+	-$(RMDIR) src/*.ll *v++* .Xil emconfig.json dltmp* xmltmp* *.log *.jou *.wcfg *.wdb
 
-# Full cleanup
 cleanall: clean
-	rm -rf _x* .Xil package.* *.csv *.json *.protoinst
+	-$(RMDIR) build_dir*
+	-$(RMDIR) package.*
+	-$(RMDIR) _x* *xclbin.run_summary qemu-memory-_* emulation _vimage pl* start_simulation.sh *.xclbin
