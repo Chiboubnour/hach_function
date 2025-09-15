83864cf1c2574379e615263df4a93528fa97a283
chiboubnour <chiboubnour32@gmail.com>
2025-09-15 09:23:23 +0200

    Update host.cpp

--- src/host.cpp
+++ src/host.cpp
@@ -109,8 +109,8 @@ int main(int argc, char* argv[]) {
     int bank_assign[2] = {0, 1}; // banques HBM pour entrée et sortie
     const char bases[4] = {'A','C','G','T'};
 
-    // ====  n = 512 ====
-    size_t n = 512;
+    // ====  n = 64 ====
+    size_t n = 64;
     std::cout << "\n=== Test avec n=" << n << " bases ===" << std::endl;
 
     std::vector<uint8_t> sequence_bytes(n);
